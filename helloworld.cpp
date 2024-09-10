1d1a7406518ff733a1705671eb46d3ecaeec5e93
tjwangchuk21 <thuksey.wangchuk@mcvts.org>
2024-09-10 10:42:50 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 3;
+    int b = 10;
+    int c = a + b;
+    float d = 1.23;
+    float j = 3.13;
+    float z = j + d;
+
+    cout << c << endl;
+    cout << "Hello World" << endl;
+    cout << z << endl;
+
+}
+
+
+
+
+
+
+
+
